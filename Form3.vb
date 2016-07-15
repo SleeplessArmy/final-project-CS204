84f42f93b913f36969becc6193f8f7117b6ab37e
SleeplessArmy <evangelistajimbo212@gmail.com>
2016-07-15 21:04:18 +0800

    Add files via upload

---
+++ Form3.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form3
+
+    
+
+    Private Sub FINISH_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FINISH.Click
+        MessageBox.Show("Next Level!", "Nice One")
+        Dim a As New Form4
+        Me.Hide()
+        a.Show()
+    End Sub
+    
+End Class
