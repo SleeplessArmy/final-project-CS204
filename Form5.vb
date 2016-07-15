ecc23e6fe629dd802508354ee55646ced6eb9378
Katlynalyn <degrano16@gmail.com>
2016-07-15 22:22:19 +0800

    Add files via upload

--- Form5.vb
+++ Form5.vb
@@ -1,10 +1,13 @@
 ﻿Public Class Form5
 
     Private Sub Form5_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        My.Computer.Audio.Play(My.Resources.Exorcist___Scream, AudioPlayMode.Background)
-        Dim t As Timer = New Timer()
+       Dim t As Timer = New Timer()
         t.Interval = 5000
         AddHandler t.Tick, AddressOf HandleTimerTick
         t.Start()
+    End Sub
+
+    Private Sub HandleTimerTick()
+        Me.Close()
     End Sub
 End Class
