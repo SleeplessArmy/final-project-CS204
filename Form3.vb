b91bb6c29d6812b54508e6fa2c9dbb4da74bae00
ClaireDizon <clairebalanzadizon@gmail.com>
2016-07-15 21:45:52 +0800

    Add files via upload

--- Form3.vb
+++ Form3.vb
@@ -1,13 +1,5 @@
 ﻿Public Class Form3
 
-    
-
-    Private Sub FINISH_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles FINISH.Click
-        MessageBox.Show("Next Level!", "Nice One")
-        Dim a As New Form4
-        Me.Hide()
-        a.Show()
-    End Sub
     Public Sub New()
 
         ' This call is required by the designer.
