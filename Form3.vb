e56fc5752814e15ed1db737d2ea347e27b802ef5
majielumbres <majielumbres@gmail.com>
2016-07-15 21:43:14 +0800

    Add files via upload

--- Form3.vb
+++ Form3.vb
@@ -17,4 +17,10 @@
         ' Add any initialization after the InitializeComponent() call.
 
     End Sub
+    Private Sub MoveToStart()
+        Dim StartingPoint = Panel1.Location
+        StartingPoint.Offset(10, 10)
+        Cursor.Position = PointToScreen(StartingPoint)
+
+    End Sub
 End Class
