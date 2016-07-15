6476ba4e3a8a69c58f118a9505408ab6d18d132f
Gerald33 <gmr09432659597@gmail.com>
2016-07-15 22:17:32 +0800

    Add files via upload

--- Form5.vb
+++ Form5.vb
@@ -4,6 +4,7 @@
         My.Computer.Audio.Play(My.Resources.Exorcist___Scream, AudioPlayMode.Background)
         Dim t As Timer = New Timer()
         t.Interval = 5000
+        AddHandler t.Tick, AddressOf HandleTimerTick
         t.Start()
     End Sub
 End Class
