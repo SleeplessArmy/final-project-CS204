7d97bda144e88d8973c0ad1a76adfb1c589c4015
Katlynalyn <degrano16@gmail.com>
2016-07-15 20:54:17 +0800

    Add files via upload

---
+++ Form2.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form2
+
+    
+    Private Sub MoveToStart()
+        Dim StartingPoint = Panel1.Location
+        StartingPoint.Offset(85, 85)
+        Cursor.Position = PointToScreen(StartingPoint)
+    End Sub
+    Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        My.Computer.Audio.Play(My.Resources.Angry_Birds_Rio_Background_music, AudioPlayMode.BackgroundLoop)
+
+    End Sub
+
+End Class
