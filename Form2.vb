5b965ff8909ab0f4d7feaaa5ac87aaaf9bbee9d6
majielumbres <majielumbres@gmail.com>
2016-07-15 20:24:36 +0800

    Add files via upload

---
+++ Form2.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form2
+
+    
+    Private Sub MoveToStart()
+        Dim StartingPoint = Panel1.Location
+        StartingPoint.Offset(85, 85)
+        Cursor.Position = PointToScreen(StartingPoint)
+    End Sub
+
+
+End Class
