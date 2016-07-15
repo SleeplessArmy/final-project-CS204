4f30bed3abe8fc96dd98eb5f6e236aa69d7d50cc
Qarinah <vertudazo.jhia@yahoo.com>
2016-07-15 19:28:52 +0800

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        If Timer1.Enabled = True Then
+            ProgressBar1.Value = ProgressBar1.Value + 3
+        End If
+        If ProgressBar1.Value = 99 Then
+            Timer1.Enabled = False
+        End If
+    End Sub
+End Class
