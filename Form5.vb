e1315572c7dde6a956418ae24b4a5c8a3f1f1591
nicasenorin2 <nicasenorin2@gmail.com>
2016-07-15 22:29:49 +0800

    Add files via upload

--- Form5.vb
+++ Form5.vb
@@ -1,12 +1,5 @@
 ﻿Public Class Form5
 
-    Private Sub Form5_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-       Dim t As Timer = New Timer()
-        t.Interval = 5000
-        AddHandler t.Tick, AddressOf HandleTimerTick
-        t.Start()
-    End Sub
-
     Private Sub HandleTimerTick()
         Me.Close()
     End Sub
