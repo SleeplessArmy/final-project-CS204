3886c6a9cdd3a7422e25287b65ce7e1f54511ebb
jopao21 <paoloplaton123456789@yahoo.com>
2016-07-16 19:52:02 +0800

    Add files via upload

--- Form1.vb
+++ Form1.vb
@@ -1,20 +1,18 @@
 ﻿Public Class Form1
 
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Timer1.Start()
+
+    End Sub
+
     Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
         If Timer1.Enabled = True Then
             ProgressBar1.Value = ProgressBar1.Value + 3
         End If
         If ProgressBar1.Value = 99 Then
             Timer1.Enabled = False
-            Dim a As New Form2
-            Me.Hide()
-            a.Show()
-
+           
         End If
-    End Sub
-
-    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        Timer1.Start()
 
     End Sub
 End Class
